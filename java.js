f484cc4a3de06f3f397463a3815374d5fa6dad55
Mark <81879524+markisik@users.noreply.github.com>
2023-10-25 23:13:59 +0200

    Update java.js

--- java.js
+++ java.js
@@ -2,7 +2,12 @@ var myVar = setInterval(function() {
   myTimer();
 }, 1000);
 
-function myTimer() {
-  var d = new Date();
-  document.getElementById("clock").innerHTML = d.toLocaleTimeString();
+window.onload = function(){
+   window.setInterval(
+	function(){
+	    var d = new Date();
+	    document.getElementById("clock").innerHTML = d.toLocaleTimeString();
+            displayCanvas();
+	}
+  , 1000);
 }
