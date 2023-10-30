c985c53326b906e79a83ebf97fd8e0cd47fecdf7
Mark <81879524+markisik@users.noreply.github.com>
2023-10-30 18:25:56 +0200

    Create java.js

---
+++ java.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+  setInterval(function() {
+    // Seconds
+    var seconds = new Date().getSeconds();
+    document.getElementById("seconds").innerHTML = (seconds < 10 ? '0' : '') + seconds;
+
+    // Minutes
+    var minutes = new Date().getMinutes();
+    document.getElementById("minutes").innerHTML = (minutes < 10 ? '0' : '') + minutes;
+
+    // Hours
+    var hours = new Date().getHours();
+    document.getElementById("hours").innerHTML = (hours < 10 ? '0' : '') + hours;
+  }, 1000);
+}
