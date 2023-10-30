2aea46a619520122367d064f4cf41e2e93d89c02
Mark <81879524+markisik@users.noreply.github.com>
2023-10-30 10:16:41 +0200

    Update

---
+++ index.js
@@ -0,0 +1,13 @@
+var myVar = setInterval(function() {
+  myTimer();
+}, 1000);
+
+window.onload = function(){
+   window.setInterval(
+        function(){
+            var d = new Date();
+            document.getElementById("clock").innerHTML = d.toLocaleTimeString();
+            displayCanvas();
+        }
+  , 1000);
+}
