8fe8726ec9ac7f81c69c31e85d51ceddab07fde4
Mark <81879524+markisik@users.noreply.github.com>
2023-11-10 22:43:25 +0200

    Add files via upload

--- java.js
+++ java.js
@@ -1,15 +1,22 @@
-window.onload = function() {
-  setInterval(function() {
-    // Seconds
-    var seconds = new Date().getSeconds();
-    document.getElementById("seconds").innerHTML = (seconds < 10 ? '0' : '') + seconds;
-
-    // Minutes
-    var minutes = new Date().getMinutes();
-    document.getElementById("minutes").innerHTML = (minutes < 10 ? '0' : '') + minutes;
-
-    // Hours
-    var hours = new Date().getHours();
-    document.getElementById("hours").innerHTML = (hours < 10 ? '0' : '') + hours;
-  }, 1000);
-}
+function setCookie(name,value,days) {
+	if (days) {
+	  var date = new Date();
+	  date.setTime(date.getTime()+(days*24*60*60*1000));
+	  var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+  }
+  function getCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+	  var c = ca[i];
+	  while (c.charAt(0)==' ') c = c.substring(1,c.length);
+	  if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+  }
+  function delCookie(name) {
+	setCookie(name,"",-1);
+  }
