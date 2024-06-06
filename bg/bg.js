a70e5857671e15e1bb0a8d441cba95fd2833c3ed
markisik <markusha00822@gmail.com>
2024-06-06 23:45:19 +0200

    Changes to be committed: modified:   bg/bg.js

--- bg/bg.js
+++ bg/bg.js
@@ -243,7 +243,7 @@
           point1 = new Point(startx, starty),
           point2 = new Point(startx, starty),
           point3 = null,
-          color = Math.round(random(35, 35, 40, 40)),
+          color = 1,
           delay = 0
   
         // buils ribbon sections
